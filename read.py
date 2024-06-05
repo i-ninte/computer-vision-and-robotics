4bed0eb1e409fbb2328f27b68edc6bde3ff748f5
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2024-06-04 22:06:15 -0700

    Delete read.py

--- read.py
+++
@@ -1,7 +0,0 @@
-'''
-in this script we learn how to read images and videos with opencv'''
-import cv2 as cv
-img= cv.imread('Photos/cat_large.jpg')
-
-cv.imshow('Cat', img)
-cv.waitKey(0) # 0 means infinite time
