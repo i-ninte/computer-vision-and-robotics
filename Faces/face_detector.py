fd109dbc95f7544a476c9a6aaa58e58e2a08d2cc
Kwabena Obeng <83280871+i-ninte@users.noreply.github.com>
2024-06-06 00:53:16 -0700

    Update face_detector.py

--- Faces/face_detector.py
+++ Faces/face_detector.py
@@ -1,1 +1,24 @@
 
+
+import cv2 as cv
+
+img = cv.imread('../Resources/Photos/group 2.jpg')
+cv.imshow('lady', img)
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Gray People', gray)
+
+haar_cascade = cv.CascadeClassifier('haar_face.xml')
+
+faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=6)
+
+print(f'Number of faces found = {len(faces_rect)}')
+
+for (x,y,w,h) in faces_rect:
+    cv.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
+
+cv.imshow('Detected Faces', img)
+
+
+
+cv.waitKey(0)
